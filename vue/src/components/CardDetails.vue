567198c6931580a0cd9aca304cc902f48e1fc645
Drew Sweet <andrewdylansweet@gmail.com>
2022-12-06 10:40:07 -0500

    made landmark details get from server

--- vue/src/components/CardDetails.vue
+++ vue/src/components/CardDetails.vue
@@ -2,13 +2,41 @@
   <div>
       <h1>{{landmark.name}}</h1>
       <p>{{landmark.description}}</p>
+      <p>Location: {{landmark.latitude}}, {{landmark.longitude}}</p>
+      <p>{{landmark.address}}</p>
+      <p>For more info: {{landmark.link}}</p>
   </div>
 </template>
 
 <script>
+
+import landmarkService from '@/services/LandmarkService.js';
+
 export default {
     name: "card-details",
-    props: ["landmark"]
+    data() {
+      return {
+        landmark: {},
+      };
+    },
+    props: ["id"],
+    created() {
+    landmarkService.getLandmarkById(this.id)
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(response.data);
+          this.landmark = response.data;
+        }
+      })
+      .catch((error) => {
+        if (error.response.status == 404) {
+          this.$router.push({ name: "NotFound" });
+        }
+      });
+      
+  },
+
+    
 }
 </script>
 
--- vue/src/views/LandmarkDetails.vue
+++ vue/src/views/LandmarkDetails.vue
@@ -1,41 +1,56 @@
 <template>
-<div>
-  <landmark-card v-bind:landmark="landmark" />
-</div>
+  <div>
+    <card-details v-bind:id="landmarkId" />
+  </div>
 </template>
 
 <script>
-import LandmarkCard from "@/components/LandmarkCard.vue";
-// import landmarkService from "../services/LandmarkService.js";
+import CardDetails from "@/components/CardDetails.vue";
+//import landmarkService from "../services/LandmarkService.js";
 
 export default {
-    name: "landmark-details",
-  components: { LandmarkCard },
-  methods: {
-    // getAllLandmarks() {
-    //   landmarkService
-    //     .getLandmarkById(this.landmark.id)
-    //     .then((response) => {
-    //       console.log("reaching");
-    //       if (response.status === 200) {
-    //         this.$store.commit("SET_LANDMARKS", response.data);
-    //         console.log("reaching");
-    //         console.log(response.data);
-    //       }
-    //     })
-    //     .catch((error) => {
-    //       if (error.response.status == 404) {
-    //         this.$router.push({ name: "NotFound" });
-    //       }
-    //     });
-    // },
+  name: "landmark-details",
+  components: { CardDetails },
+  //props: ['id'],
+  data() {
+    return { landmarkId: 0 }
   },
-  computed: {
-      landmark(){
-          const foundLandmark = this.$store.state.landmarks.find((lm) => lm.id === this.$route.params.id)
-          return foundLandmark
-      }
-  }
+  // methods: {
+  //   // getAllLandmarks() {
+  //   //   landmarkService
+  //   //     .getLandmarkById(this.landmark.id)
+  //   //     .then((response) => {
+  //   //       console.log("reaching");
+  //   //       if (response.status === 200) {
+  //   //         this.$store.commit("SET_LANDMARKS", response.data);
+  //   //         console.log("reaching");
+  //   //         console.log(response.data);
+  //   //       }
+  //   //     })
+  //   //     .catch((error) => {
+  //   //       if (error.response.status == 404) {
+  //   //         this.$router.push({ name: "NotFound" });
+  //   //       }
+  //   //     });
+  //   // },
+  // },
+  created() {
+    this.landmarkId = this.$route.params.id;
+  //   this.landmark = landmarkService.getLandmarkById(this.landmark.id)
+  //     .then((response) => {
+  //       console.log("reaching");
+  //       if (response.status === 200) {
+  //         //this.$store.commit("SET_LANDMARKS", response.data);
+  //         console.log("reaching");
+  //         console.log(response.data);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       if (error.response.status == 404) {
+  //         this.$router.push({ name: "NotFound" });
+  //       }
+  //     });
+  },
 };
 </script>
 <style></style>
