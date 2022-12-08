0631681d8563fb3fc8f926ed02dadd2e8105593a
Anthony Plouck <ploucker@gmail.com>
2022-12-08 13:44:58 -0500

    Updated Trip Controller, SqlDao, Model ALSO updated Landmark Controller, SqlDao

---
+++ dotnet/Capstone/DAO/Interfaces/ITripDao.cs
@@ -0,0 +1,10 @@
+﻿using Capstone.Models;
+using System.Collections.Generic;
+
+namespace Capstone.DAO
+{
+    public interface ITripDao
+    {
+        public List<Trip> TripsByUserId(int userId);
+    }
+}
