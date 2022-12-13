f55453aa7f5f224d310c82f34bcc5035f1957b24
Drew Sweet <andrewdylansweet@gmail.com>
2022-12-13 15:03:55 -0500

    route works

--- vue/src/components/MapboxMapAdventure.vue
+++ vue/src/components/MapboxMapAdventure.vue
@@ -1,90 +1,119 @@
 <template>
-  <MglMap :accessToken="accessToken" :mapStyle="mapStyle" :center="this.$store.state.coordinates" :zoom=12 >
 
-      <MglGeojsonLayer :sourceId="geoJsonSource.id" :source="geoJsonSource" layerId="optimized-route" :layer="geoJsonLayer" />
+  <MglMap
+    :accessToken="accessToken"
+    :mapStyle="mapStyle"
+    :center="this.$store.state.coordinates"
+    :zoom="12"
+  >
+    <MglGeojsonLayer
+      :sourceId="geoJsonSource.data.id"
+      :source="geoJsonSource"
+      layerId="myLayerId"
+      :layer="geoJsonLayer"
+    />
   </MglMap>
-
+  
 </template>
 
 <script>
 import Mapbox from "mapbox-gl";
-import {MglMap, MglGeojsonLayer} from "vue-mapbox"; //MglGeojsonLayer
+import { MglMap, MglGeojsonLayer } from "vue-mapbox"; //MglGeojsonLayer
 
 export default {
-    name: "mapbox-map",
-    components: {
-        MglMap,
-        MglGeojsonLayer,
-    },
+  name: "mapbox-map",
+  components: {
+    MglMap,
+    MglGeojsonLayer,
+  },
 
-    // methods: {
-    //     async onMapLoad(event) {
-    //         const asyncActions = event.component.asyncActions
-            
-    //         const newParams = await asyncActions.flyTo({
-    //             center: this.$store.state.coordinates,
-    //             zoom: 9,
-    //             speed: 1
-    //         })
-    //         console.log(newParams)
-    //     }
-    // },
-    data() {
-        return {
-            accessToken: 'pk.eyJ1IjoiZHJld3N3ZWV0IiwiYSI6ImNsYmgwM2kzdDBlb2MzcHFtZm8zajg2ZzIifQ.pDG4UufOD--ptSzeCeiPWA',
-            mapStyle: "mapbox://styles/mapbox/streets-v11",
-            geoJsonSource: {
-                id: 'geojson-source',
-                type: 'geojson',
-                data: {
-                    type: "FeatureCollection",
-                    features: [{
-                        type: "Feature",
-                        properties: {},
-                        geometry: {
-                            type: "LineString",
-                            coordinates: [
-                                -76.53063297271729,
-                                39.18174077994108
-                            ]
-                        }
-                    }]
-                }
-                
+  // methods: {
+  //     async onMapLoad(event) {
+  //         const asyncActions = event.component.asyncActions
+
+  //         const newParams = await asyncActions.flyTo({
+  //             center: this.$store.state.coordinates,
+  //             zoom: 9,
+  //             speed: 1
+  //         })
+  //         console.log(newParams)
+  //     }
+  // },
+  data() {
+    return {
+      accessToken:
+        "pk.eyJ1IjoiZHJld3N3ZWV0IiwiYSI6ImNsYmgwM2kzdDBlb2MzcHFtZm8zajg2ZzIifQ.pDG4UufOD--ptSzeCeiPWA",
+      mapStyle: "mapbox://styles/mapbox/streets-v11",
+      geoJsonSource: {
+        type: "geojson",
+        data: {
+          id: "test",
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+
+              geometry: {
+                type: "LineString",
+                coordinates: []
+              },
+              
             },
-            geoJsonLayer: {
-                id: 'optimized-route',
-                type: 'line',
-                source: 'geojson-source',
-                paint: {
-                    'line-color': '#3887be',
-                    'line-width': ['interpolate', ['linear'], ['zoom'], 12, 3, 22, 12]
-                },
-                
-            },
-           
-        };
-    },
-    created() {
-        this.mapbox = Mapbox;
-        console.log(this.$store.state.coordinates);
+          ],
+        },
+      },
+      geoJsonLayer: {
+        type: "line",
+        layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        paint: {
+          //"circle-color": "red",
+          'line-color': 'red',
+          'line-width': 5,
+          'line-opacity': 0.75
+        },
+      },
+    };
+  },
+  computed: {
+      optimizedCoordinates() {
+          let coordinatesArray = [];
+          for (let i = 0; i < this.$store.state.optimizedRoute.trips[0].geometry.coordinates.length; i++) {
+              coordinatesArray.push(this.$store.state.optimizedRoute.trips[0].geometry.coordinates[i]);
+          }
+          return coordinatesArray;
+      }
+  },
+  mounted() {
+    this.mapbox = Mapbox;
+    console.log(this.$store.state.optimizedRoute.trips[0].geometry);
+    console.log(this.optimizedCoordinates);
+    this.geoJsonSource.data.features[0].geometry.coordinates = this.optimizedCoordinates;
+    
 
-        console.log(this.$store.state.optimizedRoute.trips[0].geometry);
+    //this.geoJsonSource.data.features.geometry = this.$store.state.optimizedRoute.trips[0].geometry;
 
-        this.geoJsonSource.data.features.geometry = this.$store.state.optimizedRoute.trips[0].geometry;
+    // console.log(this.$store.state.coordinates);
 
-        //this.geoJsonSource = this.getSourceFeatures(this.$store.state.optimizedRoute.trips[0].geometry);
-        console.log(this.geoJsonSource);
+    // console.log(this.$store.state.optimizedRoute.trips[0].geometry);
 
-        // this.map.on('load', () => {
+    // this.geoJsonSource.data.features.geometry = this.$store.state.optimizedRoute.trips[0].geometry;
 
-        // }
+    // //this.geoJsonSource = this.getSourceFeatures(this.$store.state.optimizedRoute.trips[0].geometry);
+    // console.log(this.geoJsonSource);
+
+    // // this.map.on('load', () => {
+
+    // }
+  },
+//   updated() {
         
-    }
+//   }
+}
 
-};
 </script>
 
 <style>
-
 </style>
