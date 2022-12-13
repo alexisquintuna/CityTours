7fbefcca3d24b0e60f4e683c4e80583e6263ed64
Drew Sweet <andrewdylansweet@gmail.com>
2022-12-13 09:54:22 -0500

    figuring out routing on map

--- vue/src/services/MapboxService.js
+++ vue/src/services/MapboxService.js
@@ -11,11 +11,14 @@ export default {
     getAddressInfo(address) {
         return http.get(`/geocoding/v5/mapbox.places/${address}.json?access_token=${mapboxAccessToken}`);
     },
-    submitRoutingProblem(routingProblemDocument) {
-        return http.post(`/optimized-trips/v2?access_token=${mapboxAccessToken}`, routingProblemDocument);
-    },
-    retrieveRoutingSolution(id) {
-        return http.get(`/optimized-trips/v2/${id}?access_token=${mapboxAccessToken}`);
+    // submitRoutingProblem(routingProblemDocument) {
+    //     return http.post(`/optimized-trips/v2?access_token=${mapboxAccessToken}`, routingProblemDocument);
+    // },
+    // retrieveRoutingSolution(id) {
+    //     return http.get(`/optimized-trips/v2/${id}?access_token=${mapboxAccessToken}`);
+    // }
+    getOptimizedRoute(coordinates) {
+        return http.get(`/optimized-trips/v1/mapbox/driving/${coordinates}?geometries=geojson&destination=last&source=first&steps=true&roundtrip=false&access_token=${mapboxAccessToken}`)
     }
 }
 
